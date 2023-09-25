8fbd70159dceda6435e7b775996179b0c1b0c0f8
MenassieErmias <ermiasmenase@gmail.com>
2023-09-25 15:01:32 +0300

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+    FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithub,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare
+} from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+        <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+            <div>
+                <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+                    LANDING.
+                </h1>
+                <p className='py-4'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium facere quidem harum cum dolorem est temporibus, totam corrupti magnam beatae sapiente officiis praesentium ipsam quas. Quibusdam nihil nulla eveniet neque?
+                </p>
+                <div className='flex md:w-[75%] justify-between'>
+                    <FaFacebookSquare size={30} />
+                    <FaInstagram size={30} />
+                    <FaGithubSquare size={30} />
+                    <FaTwitterSquare size={30} />
+                    <FaDribbbleSquare size={30} />
+                </div>
+            </div>
+            <div className='lg:col-span-2 flex justify-between'>
+                <div>
+                    <h6 className='font-medium text-gray-400'>Solutions</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>Anlaytics</li>
+                        <li className='py-2 text-sm'>Marketing</li>
+                        <li className='py-2 text-sm'>Commerce</li>
+                        <li className='py-2 text-sm'>Insides</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='font-medium text-gray-400'>Support</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>Pricing</li>
+                        <li className='py-2 text-sm'>Documentation</li>
+                        <li className='py-2 text-sm'>Guides</li>
+                        <li className='py-2 text-sm'>Status</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='font-medium text-gray-400'>Company</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>About</li>
+                        <li className='py-2 text-sm'>Blog</li>
+                        <li className='py-2 text-sm'>Jobs</li>
+                        <li className='py-2 text-sm'>Press</li>
+                        <li className='py-2 text-sm'>Careers</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='font-medium text-gray-400'>Legal</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>Claim</li>
+                        <li className='py-2 text-sm'>Policy</li>
+                        <li className='py-2 text-sm'>Terms</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
